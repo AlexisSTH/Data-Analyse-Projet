ae0dc6bb1892417dbcd8ce424d1ec2c343bdd196
Alexis <sayasith.alexis@gmail.com>
2025-12-14 15:26:50 +0100

    feat: insert initial sales data into vente_chanel

---
+++ sql/insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO vente_chanel(produit, date_vente, quantite, prix, pays, canal)
+VALUES
+('Bleu de Chanel', '2023-01-15', 3, 95.00, 'France', 'Boutique'),
+('Bleu de Chanel', '2023-02-20', 2, 95.00, 'France', 'Online'),
+('Bleu de Chanel', '2023-03-10', 5, 95.00, 'USA', 'Online'),
+('Bleu de Chanel', '2023-04-05', 1, 95.00, 'USA', 'Boutique'),
+('Bleu de Chanel', '2023-05-15', 4, 95.00, 'UK', 'Online');
